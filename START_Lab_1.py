f947ea127232fe1517fc91b1e97e6891658cd06f
hnqd <dhuynh16@nait.ca>
2024-09-12 12:22:07 -0600

    my first edit -sofia

--- START_Lab_1.py
+++ START_Lab_1.py
@@ -1,29 +1,48 @@
 def lab1Question1(input_gb):
     # Convert the input of a number of gigabytes to the number of bytes
-    num_bytes = None
-    # Do the work here
-    # The solution to this goes here (and in all of them below...)
-    # Set the variable num_bytes to the answer and return it
+    input_gb = 10
+    num_bytes = 10 * (1024**3)
+    print(num_bytes)
 
     return num_bytes
 
 def lab1Question2(name):
     # Take an input of a name, return True if there is an odd number of characters in the name, False otherwise
     # Return None if the input is not a string
-    is_odd = None
+    name = 'sofie'
+    len(name) % 2 != 0 
+    if len(name) % 2 != 0:
+        print('true')
+    elif len(name) % 2 ==0:
+        print('false')
 
-    return is_odd
+    name = 'sofie'
+    if type(name) != 'str':
+        print('none')
+    elif type(name) == 'str':
+        print('nothing')
+
 
 def lab1Question3(input_string, input_number):
     # Take in two inputs - a string and a number
     # Return the character of the string in the index given by number.  If this index does not exist, then return -1.
     character_at = -1
 
+    string = 'xin chao Viet Nam'
+    number = '10'
+    two_inputs = 'string, number'
+    print(string[10])
+    
+
     return character_at
 
 def lab1Question4(file_name):
     # Take an input of a file name. 
     # Read that file and return a list of all numbers in that file
+    file_name = input('vietnam trip.txt')
+    
+
+
     list_of_nums = []
 
     return list_of_nums
