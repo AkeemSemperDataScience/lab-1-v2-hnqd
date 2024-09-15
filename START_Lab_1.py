a0a14076d0e17e158c4c609cb722429b3821d5eb
hnqd <dhuynh16@nait.ca>
2024-09-15 14:57:20 -0600

    sofia 2 edit

--- START_Lab_1.py
+++ START_Lab_1.py
@@ -1,80 +1,75 @@
 def lab1Question1(input_gb):
     # Convert the input of a number of gigabytes to the number of bytes
-    input_gb = 10
-    num_bytes = 10 * (1024**3)
-    print(num_bytes)
 
+    num_bytes = input_gb * (1024**3)
     return num_bytes
+
+# print(lab1Question1(10))
+# print(lab1Question1(5))
 
 def lab1Question2(name):
     # Take an input of a name, return True if there is an odd number of characters in the name, False otherwise
     # Return None if the input is not a string
-    name = 'sofie'
+    if name == '':
+        return None
     len(name) % 2 != 0 
     if len(name) % 2 != 0:
-        print('true')
+        return True
     elif len(name) % 2 ==0:
-        print('false')
+        return False
 
-    name = 'sofie'
-    if type(name) != 'str':
-        print('none')
-    elif type(name) == 'str':
-        print('nothing')
-
+# print(lab1Question2('sofie'))
+# print(lab1Question2('murrey'))
+# print(lab1Question2(''))
 
 def lab1Question3(input_string, input_number):
     # Take in two inputs - a string and a number
     # Return the character of the string in the index given by number.  If this index does not exist, then return -1.
     character_at = -1
-
-    string = 'xin chao Viet Nam'
-    number = '10'
-    two_inputs = 'string, number'
-    print(string[10])
-    
-
+    if not input_string:
+        return -1
+    if input_number <= len(input_string):
+        character_at = input_string[input_number]
+    else:
+        character_at = -1
     return character_at
+# print(lab1Question3('hello VietNam', 10))
+# print(lab1Question3('hello VietNam', 27))
+# print(lab1Question3('', 2))
 
 def lab1Question4(file_name):
     # Take an input of a file name. 
     # Read that file and return a list of all numbers in that file
-    file_name = input('vietnam trip.txt')
     
-
-
     list_of_nums = []
-
+    file = open(file_name, 'r')
+    information = file.readlines()
+    for each_line in information:
+        list_of_nums.append(int(each_line))
     return list_of_nums
+# print(lab1Question4('github/test_file1.txt'))
+# print(lab1Question4('github/test_file2.txt'))
+
 
 def lab1Question5(list_numbers):
     # Take an input of a list of numbers
     # Return the mode from that list. 
-    mode_of_list = None
-
+    my_set = set(list_numbers)
+    max_number = max(my_set, key = list_numbers.count)
+    mode_of_list = max_number
     return mode_of_list
+# print(lab1Question5([12,1,35,1,23,43]))
 
 def lab1Question6(quarters, dimes, nickels, pennies):
     # Take in 4 inputs - the number of quarters, dimes, nickels, and pennies in a handful
     # Return the total amount in dollars
     # For example, if the handful contains 4 quarters, 3 dimes, 2 nickels, and 1 penny, the function should return 1.41.
-    total = None
+    
+    total = (quarters* 0.25) + (dimes* 0.10) + (nickels* 0.05) + (pennies* 0.01)
     return total
+# print(lab1Question6(12,3,34,2))
 
 ## Example of calling a function to test these... 
-# Question 1 Check:
-in_gb = 10
-expected_bytes = 10*1024*1024*1024
-calculated_bytes = lab1Question1(in_gb)
-
-print("Input gigabytes: ", in_gb)
-print("Expected bytes: ", expected_bytes)
-print("Calculated bytes: ", calculated_bytes)
-if expected_bytes == calculated_bytes:
-    print("Test passed")
-else:
-    print("Test failed")
-
 # You can make similar tests to check if things work for you. 
 # This is kind of annoying, I am aware, but it is a really important skill in programming. 
 # Determining how to check if your code works, and define specific tests for what "working" means is 
